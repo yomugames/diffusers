9257b11f9dcb5c862aa6915fd11adf9613239c1b
Lawrence Tan <zymeth02@gmail.com>
2022-11-24 23:18:35 +0000

    write metadata to map image to prompt used and inference params

--- scripts/polymorphai/run-inference.py
+++ scripts/polymorphai/run-inference.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 import os
+import json
+import math
 import sys
 from IPython.display import clear_output
 from IPython.utils import capture
@@ -49,21 +51,40 @@ path_to_trained_model=SESSION_DIR+"/"+INSTANCET+'.ckpt'
 # clean output dir
 get_ipython().system('rm -rf $INF_OUTPUT_DIR')
 
-def run_inference(prompt, negative_prompt, output_dir, path_to_trained_model, ddim_steps, cfg_scale):
+def run_inference(prompt, negative_prompt, output_dir, path_to_trained_model, ddim_steps, cfg_scale, n_iter, seed):
   with capture.capture_output() as cap:
     get_ipython().run_line_magic('cd', '/content/gdrive/MyDrive/sd/stable-diffusion/')
   
-    get_ipython().system('python scripts/txt2img.py --prompt "$prompt" --negative_prompt "$negative_prompt" --seed 332 --outdir "$output_dir" --ddim_steps "$ddim_steps" --scale "$cfg_scale"  --H 512 --W 512 --n_samples 1 --n_iter 3 --skip_grid --ckpt "$path_to_trained_model"')
+    get_ipython().system('python scripts/txt2img.py --prompt "$prompt" --negative_prompt "$negative_prompt" --seed $seed --outdir "$output_dir" --ddim_steps "$ddim_steps" --scale "$cfg_scale"  --H 512 --W 512 --n_samples 1 --n_iter $n_iter --skip_grid --ckpt "$path_to_trained_model"')
 
-def rename_files(output_dir):
+def rename_files_and_write_metadata(output_dir, prompts, steps, scale, n_iter, seed):
   files = os.listdir(output_dir)
   image_files = [f for f in files if os.path.isfile(output_dir+'/'+f) and f.endswith(".png")]
+
+  metadata = { "steps": steps, "scale": scale, "seed": seed, "prompts": {} }
+
   for image_file in image_files:
+    image_index = int(image_file.replace(".png",""))
+    prompt_index = math.floor(image_index / n_iter)
+    prompt = prompts[prompt_index]
+
+    new_image_uid = str(uuid.uuid4()) 
+
     old_path = output_dir + "/" + image_file
-    new_path = output_dir + "/" + str(uuid.uuid4()) + ".png"
+    new_path = output_dir + "/" + new_image_uid + ".png"
     cmd = "mv " + old_path + " " + new_path
     print(cmd)
     get_ipython().system(cmd)
+    metadata["prompts"][new_image_uid] = prompt
+
+  print("saving image uid -> prompt map in metadata.txt")
+  metadata_path = output_dir + "/metadata.txt"
+  f = open(metadata_path, "w")
+  f.write(json.dumps(metadata))
+  f.close()
+
+
+  
 
 
 def upload_to_s3(session_name, output_dir):
@@ -93,13 +114,15 @@ ddim_configs = [10,25,40,60]
 scale_configs = [4,6,8,10,15]
 
 f = open("prompts.txt","r")
-lines = f.readlines()
+prompts = f.read().splitlines()
 f.close()
 
 outdir = INF_OUTPUT_DIR 
 
 steps = 25
 scale = 6
+n_iter = 3
+seed = 332
 
 negative_prompt = "bad anatomy, bad proportions, blurry, cloned face, deformed, disfigured, duplicate, extra arms, extra fingers, extra limbs, extra legs, fused fingers, gross proportions, long neck, malformed limbs, missing arms, missing legs, mutated hands, mutation, mutilated, morbid, out of frame, poorly drawn hands, poorly drawn face, too many fingers, ugly"
 
@@ -112,11 +135,11 @@ elif opt.gender == "Female":
 #  for scale in scale_configs:
 #    outdir = INF_OUTPUT_DIR + '/' + str(steps) + '_ddim_' + str(scale) + '_scale'
 
-for line in lines:
+for line in prompts:
   prompt = line.replace("<instance>",prompt_instance)
-  run_inference(prompt, negative_prompt, outdir, path_to_trained_model, steps, scale)
+  run_inference(prompt, negative_prompt, outdir, path_to_trained_model, steps, scale, n_iter, seed)
 
 samples_outdir = outdir + "/samples" 
-rename_files(samples_outdir)
+rename_files_and_write_metadata(samples_outdir, prompts, steps, scale, n_iter, seed)
 upload_to_s3(Session_Name, samples_outdir)
 
