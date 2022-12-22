77df89eeb878eb8f0b87b737b7ac741ef0b45043
Lawrence Tan <zymeth02@gmail.com>
2022-12-22 20:01:39 +0800

    add back neg prompt

--- scripts/polymorphai/run-inference.py
+++ scripts/polymorphai/run-inference.py
@@ -227,8 +227,7 @@ scale = 7
 n_iter = 4
 seed = 332
 
-#negative_prompt = "bad anatomy, bad proportions, blurry, cloned face, deformed, disfigured, duplicate, extra arms, extra fingers, extra limbs, extra legs, fused fingers, gross proportions, long neck, malformed limbs, missing arms, missing legs, mutated hands, mutation, mutilated, morbid, out of frame, poorly drawn hands, poorly drawn face, too many fingers, ugly"
-
+negative_prompt = "leg, feet, legs, bad anatomy, bad proportions, blurry, cloned face, deformed, disfigured, duplicate, extra arms, extra fingers, extra limbs, extra legs, fused fingers, gross proportions, long neck, malformed limbs, missing arms, missing legs, mutated hands, mutation, mutilated, morbid, out of frame, poorly drawn hands, poorly drawn face, too many fingers, ugly"
 
 if opt.gender == "Male":
   negative_prompt = "female"
